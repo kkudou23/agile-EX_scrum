81850607ab7482d5758d8d91f6a159bed05a2135
F.RN1945 <r.furukawa.sys23@morijyobi.ac.jp>
2024-08-18 03:50:29 +0900

    PlayerクラスとMainクラスを実装した close #7

---
+++ Player.java
@@ -0,0 +1,19 @@
+public class Player {
+    private int correctQuizNum;
+    public Player() {
+        this.correctQuizNum = 0;
+    }
+
+    public void showCorrectQuizNum() {
+        //TODO:実装する。
+        System.out.println("正答数:" + this.correctQuizNum);
+    }
+
+    public void setCorrectQuizNum(int correctQuizNum) {
+        this.correctQuizNum = correctQuizNum;
+    }
+
+    public int getCorrectQuizNum() {
+        return this.correctQuizNum;
+    }
+}
